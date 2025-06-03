347560244c7f907f2cd9a77c9bad194174fb52e3
Harsh Patil <itsharshhh6@gmail.com>
2025-06-03 18:20:30 +0530

    Expressions and attributes with JSX

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const name = 'Alice';
   return (
-    <>
-      
-    </>
+    <div>
+      <h1 className='greetings'>Hello, {name}!</h1>
+      <p>Welcome to the React application.</p>
+      <button ostyle={{ backgroundColor: 'blue', color: 'white' }}
+      onClick={() => alert('Button clicked!')}>
+      Click Me</button>
+      <img src="https://placehold.co/100x100" alt="image" />
+    </div>
   )
 }
 
