9fb48fb950fe9718c56c8aa47a20eb8ed22d1e14
Harsh Patil <itsharshhh6@gmail.com>
2025-06-03 18:39:52 +0530

    Conditional rendering using and operator

--- src/App.jsx
+++ src/App.jsx
@@ -12,10 +12,12 @@ function App() {
   //   </div>
   // );
   const element = <h1>{isLoggedIn ? "Welcome back" : "Please login"}</h1>
+  const messages = [];
   return (
-    <div className="App">
+    <div>
       {element}
-      <p>This is a simple React application.</p>
+      {messages.length > 0 && <h1>You have {messages.length} unread messages.</h1>}
+      {messages.length === 0 ? <h1>No new messages</h1> : null}
     </div>
   );
 }
