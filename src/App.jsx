9f77cdee31a027d279a2dde7c0875ef39eccdffa
Harsh Patil <itsharshhh6@gmail.com>
2025-06-03 18:25:59 +0530

    Conditional rendering using if else

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,16 @@
 import './App.css'
 
 function App() {
-  const name = 'Alice';
+  const isLoggedIn = false;
+  if(!isLoggedIn) {
+    return <h1>Please log in</h1>;
+  }
   return (
-    <div>
-      <h1 className='greetings'>Hello, {name}!</h1>
-      <p>Welcome to the React application.</p>
-      <button ostyle={{ backgroundColor: 'blue', color: 'white' }}
-      onClick={() => alert('Button clicked!')}>
-      Click Me</button>
-      <img src="https://placehold.co/100x100" alt="image" />
+    <div className="App">
+      <h1>Welcome to the App!</h1>
+      <p>This is a simple React application.</p>
     </div>
-  )
+  );
 }
 
 export default App
