cafbf01edd36f44eee104252a39f5b5382a97c13
Harsh Patil <itsharshhh6@gmail.com>
2025-06-03 18:34:51 +0530

    Conditional rendering using ternary

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,19 @@ import './App.css'
 
 function App() {
   const isLoggedIn = false;
-  if(!isLoggedIn) {
-    return <h1>Please log in</h1>;
-  }
+  // if(!isLoggedIn) {
+  //   return <h1>Please log in</h1>;
+  // }
+  // return (
+  //   <div className="App">
+  //     <h1>Welcome to the App!</h1>
+  //     <p>This is a simple React application.</p>
+  //   </div>
+  // );
+  const element = <h1>{isLoggedIn ? "Welcome back" : "Please login"}</h1>
   return (
     <div className="App">
-      <h1>Welcome to the App!</h1>
+      {element}
       <p>This is a simple React application.</p>
     </div>
   );
