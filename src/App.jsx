58baf0b4615c25228f9d7888ef37357e91f8aeb9
Harsh Patil <itsharshhh6@gmail.com>
2025-06-04 07:40:42 +0530

    Functions

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,19 @@
 import './App.css'
 
+function WelcomeMessage(name) {
+  return `Welcome to My React App, ${name}`;
+}
 function App() {
-  const isLoggedIn = false;
-  // if(!isLoggedIn) {
-  //   return <h1>Please log in</h1>;
-  // }
-  // return (
-  //   <div className="App">
-  //     <h1>Welcome to the App!</h1>
-  //     <p>This is a simple React application.</p>
-  //   </div>
-  // );
-  const element = <h1>{isLoggedIn ? "Welcome back" : "Please login"}</h1>
-  const messages = [];
   return (
     <div>
-      {element}
-      {messages.length > 0 && <h1>You have {messages.length} unread messages.</h1>}
-      {messages.length === 0 ? <h1>No new messages</h1> : null}
+      <h1>{WelcomeMessage('John')}</h1>
+      <h1>{WelcomeMessage('Jane')}</h1>
+      <h1>{WelcomeMessage('Alice')}</h1>
+      <h1>{WelcomeMessage('Bob')}</h1>
+      <h1>{WelcomeMessage('Charlie')}</h1>
+      <h1>{WelcomeMessage('Dave')}</h1>
+      <h1>{WelcomeMessage('Eve')}</h1>
+      <h1>{WelcomeMessage('Frank')}</h1>  
     </div>
   );
 }
